375e4b95ad480d1c7c0528df8ddcf77c6fff3e58
Adegoke Obasa <adegokeobasa@gmail.com>
2014-07-01 04:59:57 +0100

    Solved Enormous Input Problem - Code Chef

--- src/EnormousInput.java
+++ src/EnormousInput.java
@@ -1,5 +1,7 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,17 +16,35 @@ import java.util.Scanner;
 public class EnormousInput {
     static long number, counter;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
-        String line = scanner.nextLine();
-        String[] arr = line.split(" ");
-        long count = Integer.parseInt(arr[0]);
-        long divisor = Integer.valueOf(arr[1]);
-        
-        for(long i = 0; i < count; i++){
-            number = Long.parseLong(scanner.nextLine());
-            if(number % divisor == 0)
-                counter++;
+        try {
+            //        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+//        String line = scanner.nextLine();
+//        String[] arr = line.split(" ");
+//        long count = Integer.parseInt(arr[0]);
+//        long divisor = Integer.valueOf(arr[1]);
+//        
+//        for(long i = 0; i < count; i++){
+//            number = Long.parseLong(scanner.nextLine());
+//            if(number % divisor == 0)
+//                counter++;
+//        }
+//        System.out.println(counter);
+            
+            BufferedReader bInputStream = new BufferedReader(new InputStreamReader(System.in));
+            String line = bInputStream.readLine().trim();
+            String[] arr = line.split(" ");
+            long count = Integer.parseInt(arr[0]);
+            long divisor = Integer.valueOf(arr[1]);
+            
+            for(long i = 0; i < count; i++){
+                number = Long.parseLong(bInputStream.readLine().trim());
+                if(number % divisor == 0)
+                    counter++;
+            }
+            System.out.println(counter);
+        } catch (IOException ex) {
+//            Logger.getLogger(EnormousInput.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
         }
-        System.out.println(counter);
     }
 }
